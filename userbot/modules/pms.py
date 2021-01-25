181fa78f8a62e55c674b10285fde618574545678
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-01-25 23:28:52 +0530

    Add files via upload

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -27,7 +27,7 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 UNAPPROVED_MSG = (
-    "Hey there! Unfortunately, I don't accept private messages from strangers. Please contact me in a group. you will be blocked if you continue texting.\n\n If it is important and you wanna talk to me then get approval from [Kakashi](tg://user?id=1013414037)\n")
+    "Hey there! you texted me without approval.\n\n talk to[Kakashi](tg://user?id=1013414037) to get approval for my pm\n")   
 # =================================================================
 
 NO_PM_LOG_USERS = []
@@ -78,8 +78,8 @@ async def permitpm(event):
 
                 if COUNT_PM[event.chat_id] > 2:
                     await event.respond(
-                       "I told you I don't like PM \n"
-                       "you are now blocked and reported✨🦋\n"
+                       "you are now blocked and reported for spamming my pm ✨🦋\n"
+                       "have a nice day ✨✨😙\n"
                     )
 
                     try:
@@ -459,5 +459,8 @@ CMD_HELP.update({
 \n\n`logpms`\
 \nUsage: If you don't want chat logs than use `.nolog` , for opposite use `.log`. Default is .log enabled\nThis will now log chat msgs to your PM_LOGGR_BOT_API_ID.\
 \nnotice: now you can totally disable pm logs by adding heroku vars PM_LOGGR_BOT_API_ID by providing a valid group ID and NC_LOG_P_M_S True or False\
+\nwhere False means no pm logs at all..enjoy.. update and do add above mentioned vars."       
+})
+OG_P_M_S True or False\
 \nwhere False means no pm logs at all..enjoy.. update and do add above mentioned vars."       
 })
